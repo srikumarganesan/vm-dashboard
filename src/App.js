10355e40c27f90f6959a8cc4a74ed3b1794c98ef
Srikumar Ganesan <srikumarganesan@gmail.com>
2018-04-17 00:49:41 +0530

    App Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import MachineDashboard from './containers/MachineDashboard/MachineDashboard';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MachineDashboard />
+        );
+    }
 }
 
 export default App;
