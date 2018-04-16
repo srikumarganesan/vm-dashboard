11604002ab300869d1df6b85cbd6302ade8221af
Srikumar Ganesan <srikumarganesan@gmail.com>
2018-04-16 20:51:04 +0530

    Added comments

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,3 +1,10 @@
+/**
+ * This component blurs the background. It is used along with Modal to blur the background when the Modal is open
+ *
+ * @param {show} boolean value to show or hide the backdrop
+ * @param {clicked} function which gets executed when we click the backdrop
+ * @author [Srikumar Ganesan](https://github.com/srikumarganesan)
+ */
 import React from 'react';
 
 import classes from './Backdrop.css';
